e359bb194ccf1ee875772b3bc72f9e2f9a2ef106
Guillaume RICHARD <g.jf.richard@gmail.com>
2022-05-26 23:02:45 +0200

    Modification des services

--- src/Services/MdService.php
+++ src/Services/MdService.php
@@ -2,20 +2,38 @@
 
 namespace App\Services;
 
-class XmlService
+use Webuni\FrontMatter\FrontMatter;
+use League\CommonMark\CommonMarkConverter;
+
+
+
+class MdService
 {
-    public $path = "data/essence/year/PrixCarburants_annuel_";
-    public $format = ".xml";
 
-    public function __construct($year) {
-        $file = $this->path.$year.$this->format;
+    public function __construct($page) {
+        $this->file = __DIR__."/../../".$_SERVER['FILE_MD'].$page."/index.md";
 
-        if (file_exists($file)) {
-            $xml = simplexml_load_file($file);
-        } else {
-            exit('Echec lors de l\'ouverture du fichier $file.');
+        if (!file_exists($this->file)) {
+            exit('Echec lors de l\'ouverture du fichier $markdown.');
         }
+    }
 
-        return $xml->pdv;
+    public function read() {
+        $frontMatter = new FrontMatter();
+
+        $handle = fopen($this->file, 'r');
+        $markdown = fread($handle, filesize($this->file));
+
+        $hasFrontMatter = $frontMatter->exists($markdown);
+        if ($hasFrontMatter) {
+            $document = $frontMatter->parse($markdown);
+            $data = $document->getData();
+            $content = $document->getContent();
+
+            return [
+                'data'    => $data,
+                'content' => $content,
+            ];
+        }
     }
 }
--- src/Services/Xml/XmlService.php
+++ src/Services/Xml/XmlService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Xml;
 
 class XmlService
 {
