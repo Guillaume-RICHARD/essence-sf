b8b785dd8053fc99ab8169339579ca91787c862d
Guillaume RICHARD <g.jf.richard@gmail.com>
2022-05-27 22:58:09 +0200

    Lecture de markdown, logique article de blog

---
+++ public/pages/blog/articles/20220527-1805/index.md
@@ -0,0 +1,12 @@
+---
+layout: Page
+title: Blog
+description: Lorem ipsum dolor sit amet ...
+tags:
+- test
+- example
+---
+
+## Blog
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eros massa, iaculis sed lorem non, congue imperdiet leo. Vestibulum tincidunt tellus turpis, volutpat accumsan lectus pretium eget. Mauris a orci leo. Vivamus sem justo, maximus quis blandit id, ultricies id lacus. Fusce imperdiet sapien ut odio ultrices euismod. Vestibulum et metus eros. Ut purus nibh, fermentum vitae porta et, laoreet rutrum ex. Vestibulum vel dictum felis. Phasellus a quam dolor. Etiam placerat, arcu lobortis aliquam varius, purus dolor viverra lacus, ac ultrices ex turpis a felis. Integer elementum enim nisi, sed efficitur eros eleifend ut. Donec nec maximus felis, quis rhoncus ex. Nulla in enim turpis. Sed ac efficitur sapien.
--- src/Services/MdService.php
+++ src/Services/MdService.php
@@ -5,13 +5,11 @@ namespace App\Services;
 use Webuni\FrontMatter\FrontMatter;
 use League\CommonMark\CommonMarkConverter;
 
-
-
 class MdService
 {
-
     public function __construct($page) {
-        $this->file = __DIR__."/../../".$_SERVER['FILE_MD'].$page."/index.md";
+        $this->dir = __DIR__."/../../".$_SERVER['FILE_MD'].$page;
+        $this->file = $this->dir."/index.md";
 
         if (!file_exists($this->file)) {
             exit('Echec lors de l\'ouverture du fichier $markdown.');
@@ -35,5 +33,29 @@ class MdService
                 'content' => $content,
             ];
         }
+    }
+
+    public function readAllArticles() {
+        $frontMatter = new FrontMatter();
+
+        $this->folders = scandir($this->dir."/articles");
+        array_splice($this->folders,0, 2);
+
+        $articles = [];
+        foreach ($this->folders as $folder) {
+            $handle = fopen($this->dir."/articles/".$folder."/index.md", 'r');
+            $markdown = fread($handle, filesize($this->file));
+
+            $hasFrontMatter = $frontMatter->exists($markdown);
+            if ($hasFrontMatter) {
+                $document = $frontMatter->parse($markdown);
+                $articles[] = [
+                    'data' => $document->getData(),
+                    'content' => $document->getContent()
+                ];
+            }
+        }
+
+        return $articles;
     }
 }
